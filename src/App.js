9915c8b4d8eea9ee6ab6159b36c5e7be66e6c117
mateuslph <mlptche@gmail.com>
2024-03-24 15:20:39 -0300

    estiliza o header

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <Logo></Logo>
+        <ul className='opcoes'>
+          <li className='opcao'><p>CATEGORIAS</p></li>
+          <li className='opcao'><p>MINHA ESTANTE</p></li>
+          <li className='opcao'><p>FAVORITOS</p></li>
+        </ul>
       </header>
     </div>
   );
