7073dd1eab0730a2aeeba90dbc1ccd1aae112271
mateuslph <mlptche@gmail.com>
2024-03-30 12:10:08 -0300

    finaliza o projeto  falta apenas arrematar estilos

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,6 +1,8 @@
 import { livros } from './dadosUltimosLancamentos'
 import styled from 'styled-components'
 import { Titulo } from '../Titulo'
+import CardRecomenda from '../CardRecomenda'
+import imagemLivro from  '../../imagens/livro2.png'
 
 const UltimosLancamentosContainer = styled.section`
     background-color: #EBECEE;
@@ -30,6 +32,18 @@ function UltimosLancamentos() {
                     <img src={livro.src}></img>
                 ))}
             </NovosLivrosContainer>
+            <CardRecomenda 
+                titulo="Talvez você se interessa por"
+                subtitulo="Angular 11"
+                descricao="Construindo uma apicação coma a pataforma Google"
+                img={imagemLivro}
+            />
+            <CardRecomenda 
+                titulo="Talvez você se interessa por felicidade Alura"
+                subtitulo="Angular 12"
+                descricao="Construindo"
+                img={imagemLivro}
+            />
         </UltimosLancamentosContainer>
     )    
 }
