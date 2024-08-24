d7d517514c31c1c4c40dc2658205753b134c2bf9
mateuslph <mlptche@gmail.com>
2024-08-24 12:09:06 -0300

    refatora cominho log/index

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -1,4 +1,4 @@
-import Logo from '../Logo'
+import Logo from '../logo/index'
 import OpcoesHeader from '../OpcoesHeader'
 import IconesHeader from '../IconesHeader'
 import styled from 'styled-components'
